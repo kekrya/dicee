5507c0baa9e28ddc33e11bfd5f8844a2774ab016
kekrya <77329236+kekrya@users.noreply.github.com>
2021-02-25 02:44:38 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+<script>
+// Player name
+    var player1 = "Player 1";
+    var player2 = "Player 2";
+
+    // Function to roll the dice
+    function rollTheDice() {
+        setTimeout(function () {
+            var randomNumber1 = Math.floor(Math.random() * 6) + 1; //left element
+            var randomNumber2 = Math.floor(Math.random() * 6) + 1; //right element
+
+            document.querySelector(".img1").setAttribute("src",
+                "dice" + randomNumber1 + ".png");
+
+            document.querySelector(".img2").setAttribute("src",
+                "dice" + randomNumber2 + ".png");
+
+            if (randomNumber1 === randomNumber2) {
+                document.querySelector("h1").innerHTML = "Draw!";
+            }
+
+            else if (randomNumber1 < randomNumber2) {
+                document.querySelector("h1").innerHTML
+                                = (player2 + " Wins!");
+            }
+
+            else {
+                document.querySelector("h1").innerHTML
+                                = (player1 + " Wins!");
+            }
+        }, 2500);
+    }
+</script>
